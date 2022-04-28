18d8d8d67a1331aea0fda2fb628c6676ef27c1e5
Ferl Jhon D. Javier <103488259+1goadmin@users.noreply.github.com>
2022-04-28 18:02:22 +0800

    Tms upload

---
+++ wp-content/themes/tmstheme/header.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html <?= language_attributes();?>>
+<head>
+<meta charset="<?=bloginfo('charset');?>">
+<meta http-equiv="X-UA-Compatible" content="IE=edge">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta name="description" content="<?=bloginfo('Description');?>">
+<title><?=bloginfo('name');?> | <?= is_front_page()? bloginfo('description'): wp_title('');?></title>
+<meta name="description" content="Gain end-to-end visibility and manage your overall logistics in a single platform.">
+<!-- Priority -->
+<link rel="stylesheet" href="<?php echo get_template_directory_uri(); ?>/style.css"/>
+<!-- jquery -->
+<script src="https://code.jquery.com/jquery-3.6.0.slim.min.js" integrity="sha256-u7e5khyithlIdTpu22PHhENmPcRdFiHRjhAuHcs05RI=" crossorigin="anonymous"></script>
+<!-- jquery -->
+<!-- <link rel="preload" href="<=bloginfo('url');?>/wp-content/plugins/elementor/assets/css/frontend-lite.min.css" as="style"/> -->
+<!-- Priority -->
+<!-- Pre-connects -->
+<?php wp_head();?>
+<script src="https://kit.fontawesome.com/a5b3b870d7.js" crossorigin="anonymous"></script>
+</head>
+<header class="container-fluid header-active">
+    <nav class="fixed-nav-bar" id="navbar">
+    <ul class="menu">
+      <div class="logo"><a href="#"><img src="<?php echo get_template_directory_uri(); ?>/img/1golog.svg" alt=""></a></div>
+      <li class="toggle" ><a rel="nofollow" href="#" onClick='(false)' aria-label="toggle"><i class="fas fa-bars"></i></a></li>
+      <li class="item" ><a href="#">SOLUTIONS</a>  <!--class = "item sol has-submenu2"-->
+        <!-- <ul class="submenu2">
+          <li class="sub-item2"><a href="#">Outsourced TMS</a></li>
+          <li class="sub-item2"><a href="#">Enterprise TMS</a></li>
+         </ul> -->
+      </li>
+      <li class="item"><a href="#">PARTNERS</a></li>
+      <li class="item"><a href="#">CLIENTS</a></li>
+      <li class="item has-submenu" id="divi"><a rel="nofollow" tabindex="0" onClick='(href="#")'  id="div1">RESOURCES</a>
+        <ul class="submenu">
+         <li class="sub-item"><a href="#">BlOGS</a></li>
+          <li class="sub-item"><a href="#">VIDEOS</a></li>
+        </ul>
+      </li>
+      <li class="item"><a href="#">CAREERS</a></li>
+      <li class="item" id="contactUS"><a href="#">CONTACT US</a></li>
+    </ul>
+  </nav>
+</header>
+<body>
---
+++ wp-content/themes/tmstheme/index.php
@@ -0,0 +1,8 @@
+<?= get_header();?>
+<div id="main" class="container-fluid main-section">
+<div class="the_content container" id="content">
+<?php the_content(); ?>
+</div>
+</div><!-- #site-content -->
+<?php
+get_footer();?>
